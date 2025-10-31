08f09960bda3791dee58d88db3c835ec5f8a0a95
Anoop Raphel <anooparunpalliyan@gmail.com>
2025-10-31 22:19:15 +0530

    End of section 5

---
+++ client/src/features/home/home.ts
@@ -0,0 +1,18 @@
+import { Component, inject, Input, input, signal } from '@angular/core';
+import { Register } from "../account/register/register";
+import { User } from '../../types/user';
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-home',
+  imports: [Register],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+  protected registerMode = signal(false);
+  showRegister(value: boolean) {
+    this.registerMode.set(value);
+  }
+}
