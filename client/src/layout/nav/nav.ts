578c674c8e76c379cb8a293b7c038ed1af94722b
Anoop Raphel <anooparunpalliyan@gmail.com>
2025-11-16 18:42:08 +0530

    End of section 6

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,26 +2,36 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Result } from 'postcss';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
-  protected accountService= inject(AccountService)
-protected creds:any = {}
-login(){
-this.accountService.login(this.creds).subscribe({
-  next:result=>{   
-    console.log(result);
-    this.creds={};
-  },
-  error:error=>alert(error.message)
-})
-}
-logout(){
- this.accountService.logout()
-}
+  protected accountService = inject(AccountService);
+  protected router = inject(Router);
+  private toast = inject(ToastService);
+  protected creds: any = {};
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: result => {
+        this.router.navigateByUrl('/members');
+         this.toast.success('Logged in successfully');
+        console.log(result);
+        this.creds = {};
+      },
+      error: error => {
+       this.toast.error(error.error)
+      }
+    })
+  }
+  logout() {
+    
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
